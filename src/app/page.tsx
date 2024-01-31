dd9d7dd1c62ebc09de2f5a8147e3f829a3090924
Marc Chami <m.chami37@hotmail.com>
2024-01-31 16:17:50 -0500

    progressing with front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,39 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Image
-        src="/images/marcchami.jpg"
-        alt="Marc Chami"
-        width={200}
-        height={200}
-        className="rounded-full"
-      />
-      <h1 className="text-6xl font-bold mt-10">Marc Chami</h1>
-      <p className="mt-3 text-2xl">Software Engineer</p>
-      <div className="flex flex-row mt-10 space-x-4">
-        Test
-      </div>
-    </div>  );      
+export default function Page() {
+  return ( null
+    // <div className="fixed top-0 left-10 justify-center min-h-screen py-2">
+    //   <Image
+    //     src="/images/marcchami.jpg"
+    //     alt="Marc Chami"
+    //     width={200}
+    //     height={200}
+    //     // className="rounded-full"
+    //   />
+    //   <h1 className="text-6xl font-bold mt-10">Marc Chami</h1>
+    //   <p className="mt-3 text-2xl">Software Engineer</p>
+    //   <div className="flex flex-row mt-10 space-x-4">
+    //     Test
+    //   </div>
+    // </div>  
+    // <div className="flex min-h-screen" style={{ fontFamily: 'B612_Mono, monospace' }}>
+    //   <nav className="text-grey p-6">
+    //     {/* <h1 className="text-xs mb-2"><a href="#">marcchami.com </a></h1> */}
+    //       <ul>
+    //         <li className="text-xs p-2 text-gray-500"><a href="#">marcchami.com</a></li>
+    //         <li className="p-2"><a href="/about">About</a></li>
+    //         <li className="p-2"><a href="/projects">Projects</a></li>
+    //         <li className="p-2"><a href="#contact">Contact</a></li>
+    //       </ul>
+    //   </nav>
+    //   <main className=" flex-1 p-6">
+    //     <h1 className="text-4xl mt-10">Marc Chami</h1>
+    //     <p className="mt-3 text-xs">Software Engineer based in Toronto</p>
+    //     <div className="flex flex-row mt-10 space-x-4">
+    //     I am currently building and deploying infrastructure to improve overall development experience at RBC
+    //     </div>
+    //   </main>
+    // </div>
+
+    );      
 }
