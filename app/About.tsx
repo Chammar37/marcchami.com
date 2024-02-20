afc5031b374aab4cf5d0a24d69b03cc68958fac3
Marc Chami <m.chami37@hotmail.com>
2024-02-20 17:07:37 -0500

    fix socials and about page

--- app/About.tsx
+++ app/About.tsx
@@ -4,7 +4,11 @@ const About = () => {
         <div className="flex-col space-y-4"> About >
             <div className="ml-3/20 text-primary"> 
                 <p>\\ GET TO KNOW ME \\</p> <br></br> <br></br>
-                <p className=""> Born in Toronto, raised by the world</p>
+                <p className="mr-3/20"> I am committed to making a positive change in the world during my lifetime, and I believe that starts with staying persistent and immersing myself in a working environment with smart minds will help me accomplish my aspirations.</p> <br></br>
+                <p className="mr-3/20"> My background in martial arts, where I achieved a 2nd-degree black belt, and my teacher-level piano skills acquired through the Royal Conservatory of Music, have instilled in me a strong work ethic, discipline, and gratitude for every opportunity. And I desire to be the dumbest in the room, always looking to learn from others! My Moto: Nobody can tell me how to think, but everyone has a lesson to teach me - Lex Friedman</p>
+                <br></br>
+                <br></br>
+                <br></br>
             </div>
         </div>
     );
